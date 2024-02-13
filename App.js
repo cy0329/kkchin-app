c7dac02731e79157891ca8edcb7bbfaa49eca752
최열 <chlduf153@gmail.com>
2024-02-13 17:35:14 +0900

    init setting commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>내가 만든 리액트 기반 "끝까지 친다" 앱 초기 페이지</Text>
       <StatusBar style="auto" />
     </View>
   );
